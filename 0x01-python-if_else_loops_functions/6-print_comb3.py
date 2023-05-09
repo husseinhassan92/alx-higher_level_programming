2e5c0412137e0d614919fd96baf4ddd2c9c08a6f
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-05-09 08:25:55 -0700

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for j in range(10):
         if i < j:
             if i == 8 and j == 9:
-                print("{}{}".format(i,j))
+                print("{}{}".format(i, j))
             else:
-                print("{}{}, ".format(i,j),end='')
+                print("{}{}, ".format(i, j), end='')
