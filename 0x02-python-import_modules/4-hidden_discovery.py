c1c2c9970e7293e11f37cfd462301d612b6bce6b
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-05-11 16:20:05 +0300

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,8 @@
 if __name__ == "__main__":
     import hidden_4
 
-    for i in dir(hidden_4):
-        if not i.startswith(__):
+    name = dir(hidden_4)
+
+    for i in name:
+        if i[:2] != "__":
             print("{}".format(i))
