85f7b53d3a4d130df68542d994fe67254fadb251
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-05-13 17:41:44 +0300

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            print("{}".format(matrix[i][j]), end=" ")
+        for j in range(len(matrix[i])):
+            if j < (len(matrix[i]) - 1):
+                print("{}".format(matrix[i][j]), end=" ")
+            else:
+                print("{}".format(matrix[i][j]), end="")
         print()
