abd75e02420077ada9427714c34602d1d0b90a9e
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-05-17 18:03:57 +0300

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        val = sorted(a_dictionary.values())[-1]
+        for k, v in a_dictionary.items():
+            if v == val:
+                return k
