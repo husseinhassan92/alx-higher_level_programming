daa83966e0fc2125c13ec5106802793468488a80
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-05-17 18:09:49 +0300

    task 9

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary:
         val = sorted(a_dictionary.values())[-1]
         for k, v in a_dictionary.items():
             if v == val:
