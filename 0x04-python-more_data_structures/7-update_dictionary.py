32dd24013c3200099feafb4547371d16bf4a99b3
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-05-17 16:37:55 +0300

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,2 +1,7 @@
 def update_dictionary(a_dictionary, key, value):
-    return a_dictionary[key] = value
+    if a_dictionary is not None:
+        dic2 = {key: value}
+        a_dictionary.update(dic2)
+        return (a_dictionary)
+    else:
+        return None
