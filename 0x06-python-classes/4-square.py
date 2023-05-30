02f0c5ac0c1a86f9b3f6c75a99c5c4acfe13b139
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-05-30 18:08:23 +0300

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""creating square class
+"""
+
+
+class Square:
+    """class for square properties"""
+    def __init__(self, size=0):
+        """initiating square class"""
+        self.__size = size
+
+    def size(self):
+        """
+        return size of square
+        """
+        return self.__size
+
+    def size(self, value):
+        """
+        setting the size of square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
