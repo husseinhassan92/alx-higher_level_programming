8c5ee2e2b9936d0309a9eba4b1acc744c8bce4f4
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-05-30 21:41:30 +0300

    task 7

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -60,13 +60,16 @@ class SinglyLinkedList:
         new = Node(value)
         if self.__head is None:
             self.__head = new
+            return
 
         tmp = self.__head
         if new.data < tmp.data:
             new.next_node = self.__head
             self.__head = new
+            return
 
         while (tmp.next_node is not None) and (new.data > tmp.next_node.data):
             tmp = tmp.next_node
         new.next_node = tmp.next_node
         tmp.next_node = new
+        return
