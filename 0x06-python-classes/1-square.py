b4a10625f4adc8dcf864132bf06b42004a62c5aa
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-05-30 17:41:25 +0300

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""creating square class
+"""
+
+
+class Square:
+    """class for square properties"""
+    def __init__(self, size):
+        """initiating square class"""
+        self.__size = size
