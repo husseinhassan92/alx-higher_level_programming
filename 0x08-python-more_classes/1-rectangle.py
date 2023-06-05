79dc901bf8ab8c8214da253f14f2355ca042d135
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-06-05 17:09:10 +0300

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,8 +20,8 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0):
         """ Initialize rectangles """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
