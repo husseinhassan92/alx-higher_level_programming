45f374e9551befba3c2cbc7cf0c57587309baad0
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-06-05 17:00:59 +0300

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -48,6 +48,6 @@ def height(self, value):
     """
     if not isinstance(value, int):
         raise TypeError("height must be an integer")
-    elif value < 0:
+    if value < 0:
         raise ValueError("height must be >= 0")
     self.self.__height = value
