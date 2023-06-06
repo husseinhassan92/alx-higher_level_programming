491abcf62a5ca06cde1233f9beb365b9d684d6ce
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-06-06 19:04:24 +0300

    task 7

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 101-lazy_matrix_mul
+Contains method to multiply matrix using numpy module (pip3 install numpy)
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Returns matrix multiplied"""
+    return numpy.matmul(m_a, m_b)
