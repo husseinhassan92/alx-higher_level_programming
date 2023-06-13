120a73babf2d7b4f6b24a4fa50b59e27620015b1
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-06-13 19:58:36 +0300

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,15 +4,15 @@ Contains function that adds and saves to Python obj to JSON file; loads objects
 """
 
 
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import sys
+from (5-save_to_json_file) import save_to_json_file
+from (6-load_from_json_file) import load_from_json_file
 
-filename = "add_item.json"
+filename = 'add_item.json'
 
 try:
-    existing_content = load_from_json_file(filename)
+    file = load_from_json_file(filename)
 except FileNotFoundError:
-    existing_content = []
-
-save_to_json_file(existing_content + argv[1:], filename)
+    file = []
+file.extend(sys.argv[1:])
+save_to_json_file(file, filename)
