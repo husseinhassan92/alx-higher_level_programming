7cbee43d6c5d413c344c8bd5d30c3b13cefbc574
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-06-13 19:05:13 +0300

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,7 +5,7 @@ Contains function that apeends to text file and returns num chars written
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """writes to text file and returns num chars written"""
     with open(filename, 'a') as f:
         return f.write(text)
