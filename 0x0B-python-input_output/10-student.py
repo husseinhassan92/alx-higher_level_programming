fd029699c578c81941afa291b73ef63f57192e10
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-06-13 21:07:26 +0300

    task 1

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Create class Student
+"""
+
+
+class Student:
+    """
+    Public Attributes:
+        first_name
+        last_name
+        age
+    Public Methods:
+        to_json: retrieves its dictionary representation
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes student with full name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns dictionary description with simple data structure
+        for JSON serialization of an object
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            attr = {}
+            for k in attrs:
+                if k in self.__dict__.keys():
+                    attr[k] = self.__dict__[k]
+            return attr
