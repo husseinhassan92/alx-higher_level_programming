cb4b4a2fd709d2b6f59d6f2a9c1db1ba1c8e75b8
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-18 19:32:47 +0300

    task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- create a new table and fill with data
+
+CREATE TABLE second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+)
+
+INSERT INTO second_table
+VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8)
