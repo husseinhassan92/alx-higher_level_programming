2fc33fcdc9eab25f1e95832054d3ba7cb57a5048
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-18 20:09:24 +0300

    task 103

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
 -- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
 
-SELECT city, MAX(value) as max_temp
+SELECT state, MAX(value) as max_temp
 FROM temperatures
-GROUP BY city
-ORDER BY max_temp DESC;
+GROUP BY state
+ORDER BY state;
