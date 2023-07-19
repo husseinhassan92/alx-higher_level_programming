ab8c7d1f9fecd1b30d70716da12fc1dadf2b3cb1
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-19 22:26:44 +0300

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,14 @@
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+-- description of table data:
+--      id INT unique, auto generated, can’t be null and is a primary key
+--      name VARCHAR(256), can't be null
+-- should not fail if either exists
+
+CREATE DATABASE
+    IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE
+    IF NOT EXISTS hbtn_0d_usa.states(
+        id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        name VARCHAR(256) NOT NULL
+    );
