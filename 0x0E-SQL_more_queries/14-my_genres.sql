94c7308d51ef087987fabc61e1732e5e7b28612b
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-19 23:41:18 +0300

    task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -11,5 +11,5 @@ SELECT tv_genres.name
         ON tv_show_genres.genre_id = tv_genres.id
     INNER JOIN tv_shows
         ON tv_show_genres.show_id = tv_shows.id
-    GROUP BY tv_genres.name
+    WHERE tv_shows.title = "Dexter"
     ORDER BY tv_genres.name;
