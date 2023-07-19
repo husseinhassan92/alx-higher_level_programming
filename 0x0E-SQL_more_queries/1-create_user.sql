ffed7b775204d0073f364438f1cfc95d0ca1b952
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-19 22:05:32 +0300

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,6 +3,12 @@
 -- password should be set to user_0d_1_pwd
 -- if user_0d_1 already exists, script should not fail
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER 
+    IF NOT EXISTS 'user_0d_1'@'localhost' 
+    IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES 
+    ON *.* 
+    TO 'user_0d_1'@'localhost';
+
 FLUSH PRIVILEGES;
