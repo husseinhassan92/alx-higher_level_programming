703da2339ceca8bbc0f095af600a14b450176fc8
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-19 23:44:07 +0300

    task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,15 @@
+-- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+-- each record should display: tv_genres.name - number_shows
+-- don’t display a genre that doesn’t have any shows linked
+-- results must be sorted in descending order by the number of shows linked
+-- can use only one SELECT statement
+-- the database name will be passed as an argument of the mysql command
+
+SELECT tv_shows.title
+    FROM tv_shows
+    INNER JOIN tv_show_genres
+        ON tv_show_genres.show_id = tv_shows.id
+    INNER JOIN tv_genres
+        ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = "Comedy"
+    ORDER BY tv_genres.name;
