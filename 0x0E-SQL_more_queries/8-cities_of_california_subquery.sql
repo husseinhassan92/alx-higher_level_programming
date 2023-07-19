1a595f430af382939643f01226a98db520ef5c50
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-19 23:00:28 +0300

    task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,13 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+-- states table contains only one record where name = California (but the id can be different)
+-- results must be sorted in ascending order by cities.id
+-- not allowed to use the JOIN keyword
+-- the database name will be passed as an argument of the mysql comman
+
+SELECT id, name
+    FROM cities
+    WHERE state_id IN(
+        SELECT id
+        FROM states
+        WHERE name = 'California')
+    ORDER BY id;
