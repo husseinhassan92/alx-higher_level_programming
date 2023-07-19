1e72ce132a3b34692b22865f94a6507acffc9516
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-19 23:40:08 +0300

    task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -9,5 +9,7 @@ SELECT tv_genres.name
     FROM tv_genres
     INNER JOIN tv_show_genres
         ON tv_show_genres.genre_id = tv_genres.id
+    INNER JOIN tv_shows
+        ON tv_show_genres.show_id = tv_shows.id
     GROUP BY tv_genres.name
     ORDER BY tv_genres.name;
