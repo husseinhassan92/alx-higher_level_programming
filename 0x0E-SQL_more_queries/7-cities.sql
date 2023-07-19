98dd25c34e5db030df8e7aaa4bf695cb2c626ec8
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-19 22:53:24 +0300

    task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -13,6 +13,6 @@ CREATE TABLE
     IF NOT EXISTS hbtn_0d_usa.cities(
         id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
         state_id INT NOT NULL,
-        FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+        FOREIGN KEY(state_id) REFERENCES states(id),
         name VARCHAR(256) NOT NULL
     );
