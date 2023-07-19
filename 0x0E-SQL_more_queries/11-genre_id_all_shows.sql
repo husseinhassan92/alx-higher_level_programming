bae9a8d374ba1cda2e9cb84e3eb09eda611f20e4
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-19 23:18:12 +0300

    task 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,10 @@
+-- each record should display: tv_shows.title - tv_show_genres.genre_id
+-- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- can use only one SELECT statement
+-- the database name will be passed as an argument of the mysql command
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+    FROM tv_shows
+    LEFT JOIN tv_show_genres
+        ON tv_show_genres.show_id = tv_shows.id
+    ORDER BY tv_shows.title, tv_show_genres.genre_id;
