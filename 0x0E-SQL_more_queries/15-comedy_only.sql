524e7638248d5449038dfdfc1693adc63d0a59a8
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-19 23:44:46 +0300

    task 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -12,4 +12,4 @@ SELECT tv_shows.title
     INNER JOIN tv_genres
         ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_genres.name = "Comedy"
-    ORDER BY tv_genres.name;
+    ORDER BY tv_shows.title;
