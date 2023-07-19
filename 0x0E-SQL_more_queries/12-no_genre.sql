d6230d4627455ec5d7e283915cfde298493de544
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-19 23:23:53 +0300

    task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,7 +5,7 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
     FROM tv_shows
-    LEFT JOIN tv_show_genres
+    RIGHT JOIN tv_show_genres
         ON tv_show_genres.show_id = tv_shows.id
-    WHERE tv_show_genres.genre_id = NULL
+    WHERE tv_show_genres.show_id = NULL
     ORDER BY tv_shows.title, tv_show_genres.genre_id;
