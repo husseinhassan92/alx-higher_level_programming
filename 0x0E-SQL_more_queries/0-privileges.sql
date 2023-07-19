7dc73abcef6144bb89262be9b368cd8c793bee61
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-19 21:23:07 +0300

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 -- Lists all privileges of the users user_0d_1 and user_0d_2.
 
 SHOW GRANTS FOR
-    'user_0d_1'@'localhost';
+    user_0d_1@localhost;
 SHOW GRANTS FOR
-    'user_0d_2'@'localhost';
+    user_0d_2@localhost;
