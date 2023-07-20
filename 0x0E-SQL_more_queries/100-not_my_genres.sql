867456c0999c7530606c5dabecad145a65e2f271
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-20 14:06:08 +0300

    task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,13 +6,10 @@
 -- the database name will be passed as an argument of the mysql command
 
 SELECT tv_genres.name
-      FROM tv_genres
-     WHERE tv_genres.name NOT IN (
-        SELECT tv_genres.name
-        FROM tv_genres
-        INNER JOIN tv_show_genres
-            ON tv_show_genres.genre_id = tv_genres.id
-        INNER JOIN tv_shows
-            ON tv_show_genres.show_id = tv_shows.id
-        WHERE tv_shows.title = "Dexter")
+    FROM tv_genres
+    INNER JOIN tv_show_genres
+        ON tv_show_genres.genre_id = tv_genres.id
+    INNER JOIN tv_shows
+        ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title <> "Dexter"
     ORDER BY tv_genres.name;
