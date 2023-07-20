10a2ac0dee9abeabae68e6e635cffe5e23dcdc3f
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-20 14:10:36 +0300

    task 101

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,18 +1,18 @@
--- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
--- each record should display: tv_genres.name - number_shows
--- don’t display a genre that doesn’t have any shows linked
--- results must be sorted in descending order by the number of shows linked
--- can use only one SELECT statement
+-- script that lists all non-Comedy shows in the database hbtn_0d_tvshows
+-- tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- each record should display: tv_shows.title
+-- results must be sorted in ascending order by the show title
+-- can use max two SELECT statements
 -- the database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title
-    FROM tv_shows
-    WHERE tv_genres.name NOT IN(
-        SELECT tv_shows.title
-        FROM tv_shows
-        INNER JOIN tv_show_genres
-            ON tv_show_genres.show_id = tv_shows.id
-        INNER JOIN tv_genres
-            ON tv_show_genres.genre_id = tv_genres.id
-        WHERE tv_genres.name = "Comedy")
-    ORDER BY tv_shows.title;
+    SELECT tv_shows.title
+      FROM tv_shows
+     WHERE tv_shows.title NOT IN (
+    SELECT tv_shows.title
+      FROM tv_shows
+INNER JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+     WHERE tv_genres.name = "Comedy")
+  ORDER BY tv_shows.title;
