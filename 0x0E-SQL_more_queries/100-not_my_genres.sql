f22fd531265d0bb3067334e1696591f6bc975e26
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-20 14:03:46 +0300

    task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,15 +1,18 @@
--- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
--- each record should display: tv_genres.name - number_shows
--- don’t display a genre that doesn’t have any shows linked
--- results must be sorted in descending order by the number of shows linked
--- can use only one SELECT statement
+-- script that uses the hbtn_0d_tvshows database to lists all genres not linked to Dexter
+-- tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- each record should display: tv_genres.name
+-- results must be sorted in ascending order by the genre name
+-- can use max two SELECT statements
 -- the database name will be passed as an argument of the mysql command
 
 SELECT tv_genres.name
-    FROM tv_genres
-    INNER JOIN tv_show_genres
-        ON tv_show_genres.genre_id = tv_genres.id
-    INNER JOIN tv_shows
-        ON tv_show_genres.show_id = tv_shows.id
-    WHERE tv_shows.title != "Dexter"
+      FROM tv_genres
+     WHERE tv_genres.name NOT IN (
+        SELECT tv_genres.name
+        FROM tv_genres
+        INNER JOIN tv_show_genres
+            ON tv_show_genres.genre_id = tv_genres.id
+        INNER JOIN tv_shows
+            ON tv_show_genres.show_id = tv_shows.id
+        WHERE tv_shows.title = "Dexter")
     ORDER BY tv_genres.name;
