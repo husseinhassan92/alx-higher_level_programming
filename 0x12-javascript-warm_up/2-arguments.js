46699cdf5831d7feb0b257cb73408335559e5a93
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-08-14 22:38:22 +0300

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-let myVar;
-if (process.argv.length <= 2){
-    myVar = 'No argument';
-} else if (process.argv.length === 3){
-    myVar = 'Argument found';
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-    myVar = 'Arguments found';
+  console.log('Arguments found');
 }
-console.log(myVar);
