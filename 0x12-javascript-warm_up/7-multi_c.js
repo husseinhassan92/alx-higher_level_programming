cf25eee8aea1c28b485c99d7023724a7e7b53db7
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-08-14 23:34:19 +0300

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
-const num = parseInt(process.argv[2]);
-if (isNaN (num)) {
-    console.log('Missing number of occurrences');
-}
-let x = 0;
-while (x < process.argv[2]) {
+const x = parseInt(process.argv[2]);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
-    x++;
+  }
 }
