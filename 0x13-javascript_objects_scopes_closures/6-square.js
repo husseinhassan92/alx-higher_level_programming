0e0739190926adc32e8412840fff3f80ebb6c767
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-08-15 23:35:32 +0300

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const BaseSquare = require('./5-square');
+
+module.exports = class Square extends BaseSquare {
+  charPrint(c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+};
