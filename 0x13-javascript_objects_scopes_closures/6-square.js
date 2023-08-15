4fc87f5c28f622b2b94f990a4e4d7876c70d49c0
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-08-15 23:36:37 +0300

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,7 +8,7 @@ module.exports = class Square extends BaseSquare {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     }
   }
