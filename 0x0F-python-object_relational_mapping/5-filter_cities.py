ca5cf4de0ddf619b6d361b024e9c08fbe029428c
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-08-19 23:25:45 +0300

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+return all table values (table 'states')
+parameters given to script: username, password, database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3])
+    cur = conn.cursor()
+    query = """SELECT name
+                 FROM cities
+                 WHERE state_id IN (
+                 SELECT id,
+                 FROM states
+                 WHERE name = %s
+                 )"""
+    cur.execute(query, (argv[4], ))
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    conn.close()
