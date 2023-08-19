906201bbdfe9ac8a774ea2b24ebb81c7dc1942e3
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-08-19 22:53:19 +0300

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,9 @@ if __name__ == "__main__":
                            passwd=argv[2],
                            db=argv[3])
     cur = conn.cursor()
-    query = """SELECT *FROM states WHERE name LIKE {:s}
+    query = """SELECT *
+        FROM states
+        WHERE name LIKE '{:s}'
         ORDER BY id ASC""".format(argv[4])
     cur.execute(query)
     for row in cur.fetchall():
