4fb7372156e9ce8c29c8c8719d7178a6255188f6
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-08-20 23:08:13 +0300

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+return all state objects from database via python
+parameters given to script: username, password, database
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1],
+                                   argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id=2).first()
+    state.name = "New Mexico"
+    session.commit()
