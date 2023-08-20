373c109dcfc36cde569f3d86816972dfaf368445
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-08-21 00:18:56 +0300

    task 16

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+return state id given state name; SQL injection free
+parameters given to script: username, password, database, state name to match
+"""
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1],
+                                   argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
