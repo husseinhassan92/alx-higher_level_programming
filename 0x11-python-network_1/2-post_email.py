3f887c4cb205699829c5638220c2ef05aa78c973
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-02 19:59:33 +0300

    task 1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL."""
+"""Sends a POST request to a given URL with a given email."""
 import urllib.request
 import urllib.parse
 from sys import argv
