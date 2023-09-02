a488d5cc33eecd02d2a3a42f63e64b6cc8a110e1
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-02 20:15:33 +0300

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL."""
+"""given URL & email as params, display response body utf-8, print error codes"""
 import urllib.request
 from sys import argv
 
---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+fetch https://intranet.hbtn.io/status; display response
+"""
+import requests
+
+if __name__ == "__main__":
+    
