ff8c9e1c439e4b6300a4149a73327626d821a0f8
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-02 20:19:36 +0300

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,4 +5,7 @@ fetch https://intranet.hbtn.io/status; display response
 import requests
 
 if __name__ == "__main__":
-    
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
