86c838aacd5c0ca8592f1f0fae074b8ea42a27a4
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-03 08:41:13 +0300

    task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+given username and pw as param, get your id from Github api
+usage: ./10-my_github.py [github_username] [github_pw]
+"""
+
+
+from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/"
+    basic = HTTPBasicAuth(argv[1], argv[2])
+    r = requests.get(url, auth=basic)
+    print(r.json().get("id"))
