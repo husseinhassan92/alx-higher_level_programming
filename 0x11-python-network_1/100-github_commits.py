a1db9dec343eef6c5d03613f492f10e95ab8127b
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-03 09:09:34 +0300

    task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     url = 'https://api.github.com/repos/{}/{}/commits'.format(
         argv[2], argv[1])
     r = requests.get(url)
-    list_commits = r.json()
-    for commit in list_commits[0:10]:
+    commits = r.json()
+    for commit in commits[0:10]:
         print("{}: {}".format(
                 commit.get("sha"),
                 commit.get("commit").get("author").get("name")))
