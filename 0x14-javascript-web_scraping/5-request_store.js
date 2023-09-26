433e8659a9a64fd6c86b409043a9c83beea2ec4b
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-26 17:00:39 +0300

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const filename = process.argv[3];
+const fs = require('fs');
+const request = require('request');
+request(url).pipe(fs.createWriteStream(filename, 'utf-8'));
