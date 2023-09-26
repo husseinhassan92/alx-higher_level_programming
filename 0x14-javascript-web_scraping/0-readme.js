ff1ad89a3802017778ff9f3b41748561e44887bc
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-26 13:58:55 +0300

    task 1

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const filename = process.argv[2];
+const fs = require('fs');
+fs.readFile(filename, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
