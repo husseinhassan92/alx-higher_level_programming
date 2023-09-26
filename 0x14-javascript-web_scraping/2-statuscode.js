d57d21ab124cdcf4f313520e23e1db5cfd851135
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-26 14:53:39 +0300

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,6 @@
 
 const url = process.argv[2];
 const request = require('request');
-request(url, function (error, response) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('statusCode:', response && response.statusCode);
-  }
+request.get(url).on('response', function (response) {
+  console.log('code:', response.statusCode);
 });
